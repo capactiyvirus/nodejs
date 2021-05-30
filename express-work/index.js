d69809a84f77cde8a771b02b123e509654d82ae9
Dennis Sarovski <46537188+capactiyvirus@users.noreply.github.com>
2021-05-30 13:26:08 -0700

    Update index.js

--- express-work/index.js
+++ express-work/index.js
@@ -6,10 +6,10 @@ const  { Pool } = require('pg');
 const path = require('path');
 
 const pool = new Pool({
-    user: 'fuwlklnhiecfrg',
+    user: 'USER',
     host: 'ec2-54-87-112-29.compute-1.amazonaws.com',
-    database: 'd1k0s5qvr4hud',
-    password: 'e7ef70a4b0ede709fc226d7c83c753a962ae09a22a1f8658196ad602d8e4d914',
+    database: 'DB',
+    password: 'PASSWORD',
     port: 5432,
     ssl:{
         rejectUnauthorized: false,
